ae69242d5ee735c6f3cd0d8f10a6e37b043817d3
Godwin Odenigbo <godwintrav@gmail>
2023-01-19 13:24:42 +0000

    updated constraintChecker test

--- test/constraintsChecker.test.js
+++ test/constraintsChecker.test.js
@@ -12,7 +12,7 @@ describe("ConstraintsChecker Class HAPPY PATH TESTS", () => {
     expect(constraintChecker.checkMaximumTickets()).toBe(false);
   });
 
-  test("Should be check if at least 1 adult ticket is purchased with Infant and Child Tickets", () => {
+  test("Should check if at least 1 adult ticket is purchased with Infant and Child Tickets", () => {
     const ticketTypeRequest1 = new TicketTypeRequest("INFANT", 1);
     const ticketTypeRequest2 = new TicketTypeRequest("CHILD", 2);
     const constraintChecker = new ConstraintsChecker(
@@ -22,21 +22,13 @@ describe("ConstraintsChecker Class HAPPY PATH TESTS", () => {
     expect(constraintChecker.checkAdultAvailable()).toBe(false);
   });
 
-  test("Should be able to get the correct ticket price for the ticket type", () => {
-    const ticketTypeRequest1 = new TicketTypeRequest("ADULT", 2);
-    const ticketTypeRequest2 = new TicketTypeRequest("CHILD", 1);
-    const ticketTypeRequest3 = new TicketTypeRequest("INFANT", 2);
-    expect(ticketTypeRequest1.getTicketsTotalPrice()).toBe(40);
-    expect(ticketTypeRequest2.getTicketsTotalPrice()).toBe(10);
-    expect(ticketTypeRequest3.getTicketsTotalPrice()).toBe(0);
-  });
-
-  test("Should be able to get the correct ticket seats for the ticket type", () => {
-    const ticketTypeRequest1 = new TicketTypeRequest("ADULT", 2);
-    const ticketTypeRequest2 = new TicketTypeRequest("CHILD", 1);
-    const ticketTypeRequest3 = new TicketTypeRequest("INFANT", 2);
-    expect(ticketTypeRequest1.getSeats()).toBe(2);
-    expect(ticketTypeRequest2.getSeats()).toBe(1);
-    expect(ticketTypeRequest3.getSeats()).toBe(0);
+  test("Should check if number of adults is not less than number of infants", () => {
+    const ticketTypeRequest1 = new TicketTypeRequest("INFANT", 3);
+    const ticketTypeRequest2 = new TicketTypeRequest("ADULT", 1);
+    const constraintChecker = new ConstraintsChecker(
+      ticketTypeRequest1,
+      ticketTypeRequest2
+    );
+    expect(constraintChecker.checkEqualInfantAndAdult()).toBe(false);
   });
 });
